ad00730268189373f5baa0ae647dfc04d5256877
supunkamburugamuve <supun06@gmail.com>
2017-01-03 15:39:25 -0500

    New changelist

--- bin/run.sh
+++ bin/run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 jar=../target/mpi-stats-0.1-jar-with-dependencies.jar
-input=/scratch/skamburu/terasort/input/
-output=/scratch/skamburu/terasort/output/
+input=/home/supun/dev/projects/dsspidal/teragen/64/input/
+output=/home/supun/dev/projects/dsspidal/teragen/64/output/
 partitionSampleNodes=4
-partitionSamplesPerNode=100000
+partitionSamplesPerNode=10000
 filePrefix=part
 summary=summary.txt
+sendBufferSize=200000
 p=4
-
-$BUILD/bin/mpirun --report-bindings --mca btl ^tcp -np $p --hostfile nodes.txt java $opts -cp ../target/$jar edu.iu.dsc.terasort.Program -input $input -output $output -partitionSampleNodes $partitionSampleNodes -partitionSamplesPerNode $partitionSamplesPerNode -filePrefix $filePrefix 2>&1 | tee $summary
+opts="-XX:+UseG1GC -Xms3G -Xmx3G"
+$BUILD/bin/mpirun --report-bindings -np $p --hostfile nodes.txt java $opts -cp ../target/$jar edu.iu.dsc.terasort.Program2 -input $input -output $output -partitionSampleNodes $partitionSampleNodes -partitionSamplesPerNode $partitionSamplesPerNode -filePrefix $filePrefix -sendBufferSize $sendBufferSize 2>&1 | tee $summary
