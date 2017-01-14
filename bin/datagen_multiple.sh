f080d58574e88458fe62ea9020c6c388226fc6e4
Supun Kamburugamuve <supun06@gmail.com>
2017-01-14 02:46:14 -0500

    adding multiple

---
+++ bin/datagen_multiple.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+outfolder=/scratch/skamburu/terasort/input_32
+resultsfolder=/scratch/skamburu/terasort/output_32
+
+nodes="j-[035-065,077]"
+procs=10
+#records_per_node=17361112
+records_per_node=31250000
+records_per_node=15625000
+
+pdsh -w $nodes rm $outfolder/*
+#pdsh -w $nodes rm $resultsfolder/*
+start=0
+for p in $(seq 0 $procs);
+    do
+    echo pdsh -w $nodes mkdir -p $outfolder
+    pdsh -w $nodes mkdir -p $outfolder
+    pdsh -w $nodes mkdir -p $resultsfolder
+    echo pdsh -w $nodes gensort -b$start $records_per_node $outfolder/part$p
+    pdsh -w $nodes gensort -b$start $records_per_node $outfolder/part${p}_0
+    pdsh -w $nodes gensort -b$start $records_per_node $outfolder/part${p}_1
+    start=$((start + records_per_node))
+done
--- bin/run_multi.sh
+++ bin/run_multi.sh
@@ -8,7 +8,8 @@ partitionSamplesPerNode=100000
 filePrefix=part
 summary=summary.txt
 sendBufferSize=500000
-p=640
+filesPerProcess=2
+p=320
 opts="-XX:+UseG1GC -Xms12G -Xmx12G"
 #$BUILD/bin/mpirun --report-bindings --mca btl tcp,sm,self --mca btl_tcp_if_include eth1 --mca coll_tuned_use_dynamic_rules 1  --mca coll_tuned_gather_algorithm 1  -np $p --hostfile nodes.txt java $opts -cp ../target/$jar edu.iu.dsc.terasort.Program -input $input -output $output -partitionSampleNodes $partitionSampleNodes -partitionSamplesPerNode $partitionSamplesPerNode -filePrefix $filePrefix -sendBufferSize $sendBufferSize 2>&1 | tee $summary
-time $BUILD/bin/mpirun -np $p --mca btl openib,sm,self --hostfile nodes.txt java $opts -cp $jar edu.iu.dsc.terasort.Program5 -input $input -output $output -partitionSampleNodes $partitionSampleNodes -partitionSamplesPerNode $partitionSamplesPerNode -filePrefix $filePrefix -sendBufferSize $sendBufferSize 2>&1 | tee $summary
+time $BUILD/bin/mpirun -np $p --mca btl openib,sm,self --hostfile nodes.txt java $opts -cp $jar edu.iu.dsc.terasort.Program6 -input $input -output $output -partitionSampleNodes $partitionSampleNodes -partitionSamplesPerNode $partitionSamplesPerNode -filePrefix $filePrefix -sendBufferSize $sendBufferSize -filesPerProcess $filesPerProcess 2>&1 | tee $summary
