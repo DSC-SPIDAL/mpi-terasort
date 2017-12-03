f37f5876cc0bc69dce9078f16ef8d698a7418519
supunkamburugamuva <supun06@gmail.com>
2017-12-02 19:59:12 -0500

    using mem map files to write

---
+++ bin/collective/run_local.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+jar=../../target/mpi-stats-0.1-jar-with-dependencies.jar
+
+col=$1
+size=$2
+itr=$3
+p=$4
+
+opts="-XX:+UseG1GC -Xms1G -Xmx1G"
+btl="--mca btl tcp,sm,self --mca btl_tcp_if_include eth1"
+$BUILD/bin/mpirun -np $p --hostfile nodes.txt java $opts -cp ../target/$jar edu.iu.dsc.collectives.Program -collective $col -size $size -itr $itr  2>&1 | tee $summary
--- src/main/java/edu/iu/dsc/stats/StatProgram.java
+++ src/main/java/edu/iu/dsc/stats/StatProgram.java
@@ -1,7 +1,17 @@
 package edu.iu.dsc.stats;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatProgram {
   public static void main(String[] args) {
-
+    int partitionSize = 100;
+    List<Double> numbers = new ArrayList<>();
+    double sum = 0, average = 0;
+    for (int i = 0; i < partitionSize; i++) {
+      sum += numbers.get(0);
+    }
+    average = sum / partitionSize;
+    
   }
 }
